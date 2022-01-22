cb90fd05930ebd8bcfa07cc47298f16622b24f32
Vijay Koladara <abc.vijaykoladara212@gmail.com>
2022-01-22 12:46:46 +0530

    All Done

---
+++ src/redux/actions/index.ts
@@ -0,0 +1,72 @@
+import * as types from "../constants/index";
+import axios from "axios";
+import { Dispatch } from "redux";
+
+import { AddPost, Post, Props } from "../../models/redux";
+import { API_URL } from "../../shared/constant";
+
+const getPosts = (posts: Props) => ({
+  type: types.GET_POSTS,
+  payload: posts,
+});
+
+const postDeleted = (id: number) => ({
+  type: types.DELETE_POST,
+  payload: id,
+});
+
+const postAdded = (post: AddPost) => ({
+  type: types.ADD_POST,
+  payload: post,
+});
+
+const postUpdated = (post: Post) => ({
+  type: types.UPDATE_POST,
+  payload: post,
+});
+
+const setSelectedPost = (post: Post) => ({
+  type: types.SET_SELECTED_POST,
+  payload: post,
+});
+
+export const getAllPosts = () => {
+  return async function (dispatch: Dispatch) {
+    const response = await axios.get(`${API_URL}/posts`);
+    dispatch(getPosts(response.data));
+  };
+};
+
+export const deletePost = (id: number) => {
+  return async function (dispatch: Dispatch) {
+    await axios.delete(`${API_URL}/posts/${id}`);
+    dispatch(postDeleted(id));
+  };
+};
+
+export const addPost = (post: AddPost) => {
+  return async function (dispatch: Dispatch) {
+    await axios.post(`${API_URL}/posts`, post);
+    dispatch(postAdded(post));
+  };
+};
+
+export const getPostById = (id: number) => {
+  return async function (dispatch: Dispatch) {
+    const response = await axios.get(`${API_URL}/posts/${id}`);
+    dispatch(setSelectedPost(response.data));
+  };
+};
+
+export const updatePost = (id: number, post: Post) => {
+  return async function (dispatch: Dispatch) {
+    await axios.put(`${API_URL}/posts/${id}`);
+    dispatch(postUpdated(post));
+  };
+};
+
+export const setPost = (post: Post) => {
+  return async function (dispatch: Dispatch) {
+    dispatch(setSelectedPost(post));
+  };
+};
